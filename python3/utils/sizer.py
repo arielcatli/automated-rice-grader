7e9e686e26ea54147e47473cb9f4e255676b9dd3
Ariel Catli <arielcatli@gmail.com>
2018-03-17 14:10:20 +0800

    Revert "grader"
    This reverts commit a899e34cd85f5669c62c2c740e1ce21c18199eb6.

--- python3/utils/sizer.py
+++ python3/utils/sizer.py
@@ -17,30 +17,14 @@ class Sizer:
         self.avg_size = None
         self.unitPerPixel = 0
         
-    def add_images(self, sample_directory):
-        images = os.listdir(sample_directory)
+    def add_images(self, images_directory):
+        images = os.listdir(images_directory)
         self.grain_count+= len(images)
         for image in images:
-            file_name = sample_directory+"/"+image
-            image = cv2.imread(file_name,0)
-            thresh, binary_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-            binary_image = cv2.Canny(cv2.GaussianBlur(binary_image, (31,31), 0), 30, 150)
-            contours = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-            max_height = 0
-            max_contour = 0
-            for i,contour in enumerate(contours[1]):
-                x,y,w,h = cv2.boundingRect(contour)
-                if max_height < h:
-                    max_height = h
-                    max_contour = contour
-                    
-            x,y,w,h = cv2.boundingRect(max_contour)
-            self.__sum += h
+            image_ = cv2.imread(images_directory+image)
+            self.__sum += image_.shape[0] 
             
-            
-            
-    def average_size(self):
+    def size_up(self, report_directory):
         if self.__isCalibrated and self.grain_count != 0:
             self.avg_size = self.__sum/self.grain_count
             return self.avg_size*self.unitPerPixel
@@ -63,7 +47,7 @@ class Sizer:
                 max_height = h
                 max_contour = contour
                 
-        x,y,w,h = cv2.boundingRect(max_contour)  
+        x,y,w,h = cv2.boundingRect(max_contour)        
         self.__isCalibrated = True
         unitPerPixel = object_size/h
         self.unitPerPixel = unitPerPixel
@@ -72,15 +56,12 @@ class Sizer:
     def generate_report(self, report_directory):
         with open(report_directory, "wb") as report:
             pickle.dump([self.avg_size, self.grain_count, self.unitPerPixel], report)
-            
-    def pixelToMm(self, pixel):
-        return pixel*self.unitPerPixel
         
         
 if __name__ == "__main__":
     sizer = Sizer()    
     #sizer.add_images("../img-src/38/extracted/p/")
-    sizer.calibrate("../../coin.jpg", 24)
+    sizer.calibrate("../coin.jpg", 24)
     #print(sizer.size_up("dfs"))
     #sizer.generate_report("../img-src/38/extracted/report.bin")
     
